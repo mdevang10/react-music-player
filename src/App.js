ed3a424ce766cef9c4cc7966881a82d36b7efc52
Devang Mistry <devang.mistry@somaiya.edu>
2021-04-04 15:08:48 +0530

    added-library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import "./styles/app.scss"
-import Song from "./components/Song"
+import "./styles/app.scss";
+import Song from "./components/Song";
 import Player from "./components/Player";
+import Library from "./components/Library";
 
-import data from "./util"
+import data from "./util";
 
 function App() {
-
     const [songs, setSongs] = useState(data());
     const [currentSong, setCurrentSong] = useState(songs[0]);
     const [isPlaying, setIsPlaying] = useState(false);
@@ -18,8 +18,9 @@ function App() {
             <Player
                 currentSong={currentSong}
                 isPlaying={isPlaying}
-                setIsPlaying={setIsPlaying}>
-                </Player>
+                setIsPlaying={setIsPlaying}
+            ></Player>
+            <Library songs={songs}></Library>
         </div>
     );
 }
